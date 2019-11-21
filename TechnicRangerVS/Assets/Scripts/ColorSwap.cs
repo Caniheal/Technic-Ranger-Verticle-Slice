597306fc2f9f2a329614be60551533874205a124
Caniheal <37191567+Caniheal@users.noreply.github.com>
2019-11-21 17:02:30 -0500

    Art scripts
    idk if we need these but here they are

---
+++ TechnicRangerVS/Assets/Scripts/ColorSwap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSwap : MonoBehaviour
+{
+    public Material Purple, Teal, Tan, Blue, White;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GetComponent<Renderer>().material = Tan;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            GetComponent<Renderer>().material = Tan;
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            GetComponent<Renderer>().material = Purple;
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            GetComponent<Renderer>().material = Teal;
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            GetComponent<Renderer>().material = White;
+        }
+        if (Input.GetKeyDown("5"))
+        {
+            GetComponent<Renderer>().material = Blue;
+        }
+    }
+}
---
+++ TechnicRangerVS/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Start()
+    {
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Exit");
+    }
+}
