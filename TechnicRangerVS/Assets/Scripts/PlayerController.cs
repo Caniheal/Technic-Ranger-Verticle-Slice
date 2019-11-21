c7214da6c0d94c082e8fc4759d860b517252114c
Caniheal <37191567+Caniheal@users.noreply.github.com>
2019-11-21 16:29:29 -0500

    Scripts from tech proof

---
+++ TechnicRangerVS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    public float MouseSensitivity = 1;
+    public float CameraDistance = 3;
+    public float CameraSphereTestRadius = .3f;
+    public Vector3 CameraOffset;
+
+    public float MinCameraDistance = .1f;
+    public float Gravity = 9.8f;
+    public float FallingMultiplier = 2.5f;
+    public float JumpingMultiplier = 2.0f;
+    public float AirControlMultiplier = .5f;
+
+    public float WarpFirstTestRadius = .3f;
+    public float WarpSecondTestRadius = 1;
+
+    public float JumpSpeed = 8;
+
+    public Camera Camera;
+    public WarpManager WarpManager;
+
+    //Welcome to Lylly's notes in the script. :)
+    // Euler Angle (rotation) is when... x = pitch; y = yaw; z= roll
+    public float Yaw;
+    public float Pitch;
+    public Vector3 MoveDirection;
+
+    private CharacterController characterController;
+    private bool movementEnabled = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    public void DisableMovement()
+    {
+        movementEnabled = false;
+        characterController.enabled = false;
+    }
+
+    public void EnableMovement()
+    {
+        movementEnabled = true;
+        characterController.enabled = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement();
+        UpdateCamera();
+        UpdateWarper();
+    }
+
+
+    void UpdateMovement()
+    {
+        if (!movementEnabled)
+        {
+            return;
+        }
+
+        //Our "forward" is the same as the camera
+        //Zero out the y so you don't fly up or fall down 
+        Vector3 forward = Camera.transform.forward;
+        forward.y = 0;
+
+
+        Vector3 right = Camera.transform.right;
+        right.y = 0;
+
+        //Default when you're not moving
+        MoveDirection = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveDirection += forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //back
+            MoveDirection += -forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //left
+            MoveDirection += -right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoveDirection += right;
+        }
+
+        MoveDirection = MoveDirection * MovementSpeed;
+
+        //How far you can move when in air
+        //! = not
+        if (!characterController.isGrounded)
+        {
+            MoveDirection = MoveDirection * AirControlMultiplier;
+        }
+
+        // jump
+        if (characterController.isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            MoveDirection += Vector3.up * JumpSpeed;
+        }
+
+        //current character velocity
+        if (characterController.velocity.y < 0)
+        {
+            //Down
+            MoveDirection += Vector3.up * Gravity * FallingMultiplier * Time.deltaTime;
+        }
+        else
+        {
+            //Up
+            MoveDirection += Vector3.up * Gravity * JumpingMultiplier * Time.deltaTime;
+        }
+
+        //maintain up/down velocity; continue to move the way ya moving
+        MoveDirection.y += characterController.velocity.y;
+
+        //Telling CharacterConroller to move in this direction (Also moveDirec*moveSpeed = velocity)
+        characterController.Move(MoveDirection * Time.deltaTime);
+    }
+
+
+
+    void UpdateCamera()
+    {
+        //Keep yaw between -180 and 180 (360 camera rotation)
+        //adding to yaw
+        Yaw += MouseSensitivity * Input.GetAxis("Mouse X");
+
+        //Keep pitch between -90 and 90 (180 camera rotation) to not flip backwards
+        //pitch gets inverted so subtract
+        Pitch -= MouseSensitivity * Input.GetAxis("Mouse Y");
+        Pitch = Mathf.Clamp(Pitch, -90, 90);
+
+        //make rotation from euler angles
+        Quaternion rotation = Quaternion.Euler(Pitch, Yaw, 0);
+
+
+
+        Vector3 focusPosition = CameraOffset + transform.position;
+
+        //                          capsule position + rotation of mouse * back(moving camera back so it's not inside you)
+        Vector3 desiredCameraPosition = focusPosition + rotation * Vector3.back * CameraDistance;
+
+        Vector3 cameraPosition = Camera.transform.position;
+        Vector3 capsuleToCameraDirection = (desiredCameraPosition - focusPosition).normalized;
+
+        //calculating distance between 2 points; float = the value
+        float cameraMoveDistance = Vector3.Distance(focusPosition, desiredCameraPosition);
+
+        //output of the raycast/spherecast 
+        RaycastHit hit;
+
+
+        //Moves the camera(thats in desired location) to the hit point 
+        if (Physics.SphereCast(focusPosition, CameraSphereTestRadius, capsuleToCameraDirection, out hit, cameraMoveDistance))
+        {
+            desiredCameraPosition = hit.point;
+
+            if (hit.distance < MinCameraDistance)
+            {
+                desiredCameraPosition = Camera.transform.position;
+            }
+        }
+
+
+        Camera.transform.position = desiredCameraPosition;
+
+        //cameraToCapsule = way the camera is looking to the character
+        //                        capsule position - (ABOVE; camera position) ....normalized(makes everything between 0-1 <--  so we know the DIRECTION #PHYSICS)
+        Vector3 cameraToCapsuleDirection = (focusPosition - Camera.transform.position).normalized;
+
+        //Direction lets us compute a rotation(camera) from that direction; rotating camera to face capsule
+        //idk why it's .up but it told me to write .up so i will write .up
+        Camera.transform.rotation = Quaternion.LookRotation(cameraToCapsuleDirection, Vector3.up);
+    }
+
+    void UpdateWarper()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Vector3 CameraDirection = Camera.transform.rotation * Vector3.forward;
+            RaycastHit hit;
+
+            Debug.DrawLine(Camera.transform.position, Camera.transform.position + CameraDirection * 100, Color.red, 30);
+
+            if (Physics.SphereCast(Camera.transform.position, WarpFirstTestRadius, CameraDirection, out hit, 10f))
+            {
+                CameraDirection.y = 0;
+          
+                WarpManager.PlaceWarper(hit.point, CameraDirection);
+            }
+            else 
+            {
+                // no warping cast sound
+            }
+        }
+    }
+}
---
+++ TechnicRangerVS/Assets/Scripts/WarpManager.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpManager : MonoBehaviour
+{
+    public GameObject StartWarper;
+    public GameObject EndWarper;
+
+    public Transform warpRealmStartPosition;
+    public Transform warpRealmEndPosition;
+    public Material RealmSkybox;
+    public Material NormalSkybox;
+
+    //Camera roation; ROTATION = QUATERNION
+    public Quaternion warpRealmCameraOrientation;
+    public float WarpTime = 1;
+    public float DistanceToWarp = 1;
+
+    //How big sphere cast balls are ;D
+    public float WarperTestRadius = 1;
+    //How long sphere cast balls are ;D
+    public float WarpTestDistance = 3;
+
+    //This is the warp; not in use
+    private bool isActive = false;
+    private PlayerController player;
+
+    private float currentTime = 0;
+
+    void Start()
+    {
+        player = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            currentTime += Time.deltaTime;
+
+         //Need this ratio to go from 0-1 (total distance)
+            float lerpRatio = currentTime / WarpTime;
+
+        //Lerp ratio; EXAMPLE; percentage between start/end values
+            // float start = 10;
+            //float end = 20;
+            // lerpRatio(10, 20, 0) = 10;
+            //lerpRatio(10, 20, .5) = 15;
+            //lerpRatio(10, 20, 1) = 20;
+
+
+            Vector3 currentPostion = Vector3.Lerp(warpRealmStartPosition.position, warpRealmEndPosition.position, lerpRatio);
+
+            player.gameObject.transform.position = currentPostion;
+
+            if (currentTime >= WarpTime)
+            {
+                //current time larger than 1
+                isActive = false;
+                Vector3 EndPostion = EndWarper.transform.position;
+                EndPostion.y = 1.1f;
+
+                player.gameObject.transform.position = EndPostion;
+            
+                player.EnableMovement();
+                RenderSettings.skybox = NormalSkybox;
+            }
+        }
+
+        //distance from the player to the startwarper 
+        float DistanceToStartWarper = Vector3.Distance(StartWarper.transform.position, player.transform.position);
+
+        Debug.Log(DistanceToStartWarper);
+        if (DistanceToStartWarper < DistanceToWarp)
+        {
+            Warp();
+        }
+    }
+
+    //public lets other scripts call the function
+    //Where the warper is going to be placed in the world
+    public void PlaceWarper(Vector3 placePosition, Vector3 placedDirection)
+    {
+        RaycastHit hit;
+                                                                   //multiply so we can move that distance (in that direction)
+        Vector3 endWarperLocation = placePosition + placedDirection * WarpTestDistance;
+
+        //Where the warper is places, how big the sphere cast is, what direction we're testing, reported hits (walls), how long the sphere cast is
+        if (Physics.SphereCast(placePosition, WarperTestRadius, placedDirection, out hit, WarpTestDistance))
+        {
+            //if we hit a wall, put end warper at that wall
+            endWarperLocation = hit.point;
+        }
+
+        StartWarper.SetActive(true);
+        EndWarper.SetActive(true);
+
+        StartWarper.transform.position = placePosition;
+        EndWarper.transform.position = endWarperLocation;
+    }
+
+    //Warp activated; in use
+    void Warp()
+    {
+        isActive = true;
+        player.DisableMovement();
+        RenderSettings.skybox = RealmSkybox;
+
+        //Teleporting player to the REALM start position
+        player.gameObject.transform.position = warpRealmStartPosition.position;
+
+        //setting camera orientation to the warp realm orientation
+        player.Camera.gameObject.transform.rotation = warpRealmCameraOrientation;
+
+        //current time in realm always starts at 0
+        currentTime = 0;
+    }
+
+}
