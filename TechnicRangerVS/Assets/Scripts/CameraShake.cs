cdf7fbb71af722ecb75a72fdda23184a90561c6d
MrJuniorice <43075764+MrJuniorice@users.noreply.github.com>
2019-11-26 16:51:47 -0500

    Grappling
    I need help making the grappling hook pull items to the character

---
+++ TechnicRangerVS/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 originalPosition = transform.localPosition;
+        float elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.GetComponent.camera;
+        }
+    }
+}
