9cd3a526ece95a22139eb7ca6ffb7f7e1654ee69
MrJuniorice <43075764+MrJuniorice@users.noreply.github.com>
2019-12-03 17:20:26 -0500

    Added Gamma Script

---
+++ TechnicRangerVS/Assets/Gamma.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamma : MonoBehaviour
+{
+    float rgbValue = .5f;
+    void OnGUI()
+    {
+        rgbValue = GUI.HorizontalSlider (new Rect (Screen.width/2 - 50,90,100,30), rgbValue, 0f,1.0f);
+        RenderSettings.ambientLight = new Color(rgbValue, rgbValue, rgbValue, 1);
+    }
+}
