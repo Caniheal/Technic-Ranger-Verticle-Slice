0cca61a14cad0aa08f2ee40b30668b38f9a93487
FranLenny <42728905+FranLenny@users.noreply.github.com>
2019-12-04 12:08:28 -0500

    Checkpoint scripts

---
+++ TechnicRangerVS/Assets/Scripts/ChangeCheckpoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ChangeCheckpoint : MonoBehaviour
+{
+    public GameObject checkpoint;
+
+    void OnTriggerEnter(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Player")
+            Destroy(checkpoint);
+        Destroy(gameObject);
+    }
+}
---
+++ TechnicRangerVS/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+
+    public Transform checkpoint;
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void OnTriggerEnter (Collider plyr)
+    {
+        if(plyr.gameObject.tag == "Player")
+        {
+            player.transform.position = checkpoint.position;
+            player.transform.rotation = checkpoint.rotation;
+        }
+    }
+}
