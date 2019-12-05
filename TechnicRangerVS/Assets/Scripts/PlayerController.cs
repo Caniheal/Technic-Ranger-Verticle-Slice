4e447c10f94e34d958c037b9456dde8ff1a2372f
Caniheal <37191567+Caniheal@users.noreply.github.com>
2019-12-05 07:53:44 -0500

    Revert "Merge branch 'master' of https://github.com/Caniheal/Technic-Ranger-Verticle-Slice"
    This reverts commit bf82d545a7c5c258eb58f78e46ad931dfa0eed22, reversing
    changes made to 671c5a1e8955a89b2638a92f423872897a12ec04.

--- TechnicRangerVS/Assets/Scripts/PlayerController.cs
+++ TechnicRangerVS/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //For our animations
-    private Animator anim;
-
     public float MovementSpeed = 1;
     public float MouseSensitivity = 1;
     public float CameraDistance = 3;
@@ -39,8 +36,6 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
-
         characterController = GetComponent<CharacterController>();
     }
 
@@ -102,18 +97,7 @@ public class PlayerController : MonoBehaviour
             MoveDirection += right;
         }
 
-        //Animations
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
-        {
-            anim.SetBool("isRunning", true);
-        }
-        else
-        {
-            anim.SetBool("isRunning", false);
-        }
-            
-
-            MoveDirection = MoveDirection * MovementSpeed;
+        MoveDirection = MoveDirection * MovementSpeed;
 
         //How far you can move when in air
         //! = not
