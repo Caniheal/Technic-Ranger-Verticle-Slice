6e4b3ead12e456f218609921174cd7931f704740
Caniheal <37191567+Caniheal@users.noreply.github.com>
2019-12-05 04:10:21 -0500

    Coin must die

---
+++ TechnicRangerVS/Assets/Scripts/CoinKiller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinKiller : MonoBehaviour
+{
+    public AudioSource sound;
+    public AudioClip SoundToPlay;
+    public float Volume;
+    public bool alreadyPlayed = false;
+
+
+    private void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+   
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("workplz");
+
+        if (!alreadyPlayed)
+        {
+            sound.PlayOneShot(SoundToPlay, Volume);
+            alreadyPlayed = true;
+        }
+
+        Object.Destroy(gameObject);
+
+     
+            
+        }
+   
+}
+
