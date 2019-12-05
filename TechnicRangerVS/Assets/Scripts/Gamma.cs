16a2d5e16c2dce6c631297f6120a1d17f32a7762
MrJuniorice <43075764+MrJuniorice@users.noreply.github.com>
2019-12-05 16:07:38 -0500

    Settings Menu

--- TechnicRangerVS/Assets/Scripts/Gamma.cs
+++ TechnicRangerVS/Assets/Scripts/Gamma.cs
@@ -7,7 +7,7 @@ public class Gamma : MonoBehaviour
     float rgbValue = .5f;
     void OnGUI()
     {
-        rgbValue = GUI.HorizontalSlider (new Rect (Screen.width/2 - 50,90,100,30), rgbValue, 0f,1.0f);
+        rgbValue = GUI.HorizontalSlider (new Rect (Screen.width/3 - 200,275,100,25), rgbValue, 0f,1.0f);
         RenderSettings.ambientLight = new Color(rgbValue, rgbValue, rgbValue, 1);
     }
 }
