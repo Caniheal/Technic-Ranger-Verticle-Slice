03c1bf51b7017edff36d3077ea82864ffdefebcd
FranLenny <42728905+FranLenny@users.noreply.github.com>
2019-12-05 00:29:24 -0500

    Lighting in mainscene

---
+++ TechnicRangerVS/Assets/Scripts/Death.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    
+    public Transform startPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("player"))
+        {
+            other.gameObject.transform.position = startPos.position;
+        }
+    }
+}
