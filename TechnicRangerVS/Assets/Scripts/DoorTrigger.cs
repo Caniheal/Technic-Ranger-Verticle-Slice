c977ad470b992b628d69ddf93ae61a1351f78bec
Caniheal <37191567+Caniheal@users.noreply.github.com>
2019-12-04 23:30:46 -0500

    door trigger

--- TechnicRangerVS/Assets/Scripts/DoorTrigger.cs
+++ TechnicRangerVS/Assets/Scripts/DoorTrigger.cs
@@ -7,12 +7,17 @@ public class DoorTrigger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+   
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("working");
     }
 }
