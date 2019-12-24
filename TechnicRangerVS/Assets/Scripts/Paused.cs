efeddf12a55f008ab5cf116cc3ca76311fab35ce
MrJuniorice <43075764+MrJuniorice@users.noreply.github.com>
2019-12-24 10:06:01 -0500

    Pause Menu 90% working

--- TechnicRangerVS/Assets/Scripts/Paused.cs
+++ TechnicRangerVS/Assets/Scripts/Paused.cs
@@ -1,37 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Paused : MonoBehaviour
 {
-    public static bool GameIsPaused = false;
+    [SerializeField] private bool GameIsPaused;
 
-    public GameObject pauseMenuUI;
+    [SerializeField] public GameObject pauseMenuUI;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Start"))
         {
-            if (GameIsPaused)
-            {
-                Resume();
-            }else
-            {
-                Pause();
-            }
+            GameIsPaused = !GameIsPaused;
+        }
+
+        if (GameIsPaused)
+        {
+            Pause();
+        }
+        else
+        {
+            Resume();
         }
     }
 
-    void Resume()
+    public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        AudioListener.pause = false;
         Time.timeScale = 1f;
+        GameIsPaused = false;
     }
 
     void Pause()
     {
         pauseMenuUI.SetActive(true);
+        AudioListener.pause = true;
         Time.timeScale = 0f;
-        GameIsPaused = true;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
     }
 }
