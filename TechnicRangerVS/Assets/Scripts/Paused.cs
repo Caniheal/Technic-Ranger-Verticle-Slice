09174f4f9e0c0136565d4f06300f239c3a0c0411
MrJuniorice <43075764+MrJuniorice@users.noreply.github.com>
2020-01-09 17:19:34 -0500

    Pause Menu Changes

--- TechnicRangerVS/Assets/Scripts/Paused.cs
+++ TechnicRangerVS/Assets/Scripts/Paused.cs
@@ -6,26 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class Paused : MonoBehaviour
 {
-    [SerializeField] private bool GameIsPaused;
+    private static bool GameIsPaused = false;
 
-    [SerializeField] public GameObject pauseMenuUI;
+    public GameObject pauseMenuUI;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Start"))
         {
-            GameIsPaused = !GameIsPaused;
-        }
-
-        if (GameIsPaused)
-        {
-            Pause();
-        }
-        else
-        {
-            Resume();
+            if (GameIsPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
         }
     }
+
+        
 
     public void Resume()
     {
@@ -40,6 +40,7 @@ public class Paused : MonoBehaviour
         pauseMenuUI.SetActive(true);
         AudioListener.pause = true;
         Time.timeScale = 0f;
+        GameIsPaused = true;
     }
 
     public void MainMenu()
