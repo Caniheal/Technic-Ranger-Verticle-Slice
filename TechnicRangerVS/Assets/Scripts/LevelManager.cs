caf93566bc26c2aa5b39fb6a5fc23ec2ef347afa
Caniheal <37191567+Caniheal@users.noreply.github.com>
2020-01-14 12:48:30 -0500

    made a new script to navigate the menus
    not implimented yet will finish by end fo teh night

---
+++ TechnicRangerVS/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void FinishGame()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("backup2");
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("test2");
+
+        if (other.gameObject.tag == ("Player"))
+        {
+            Invoke("FinishGame", 5);
+
+            Debug.Log("test");
+        }
+
+    }
+}
