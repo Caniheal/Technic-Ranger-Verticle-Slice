535333b9c8062661a0d23ad0917d20e6b9dd3931
MrJuniorice <43075764+MrJuniorice@users.noreply.github.com>
2020-02-11 17:12:13 -0500

    More Controller Support for Masks

--- TechnicRangerVS/Assets/Scripts/PlayerController.cs
+++ TechnicRangerVS/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public enum WeaponState
 public class PlayerController : MonoBehaviour
 {
     //For our animations
+    private float DpadX;
+    private float DpadY;
     private Animator anim;
 
     public float MovementSpeed = 1;
@@ -307,6 +309,10 @@ public class PlayerController : MonoBehaviour
 
     void UpdateWeapon()
     {
+
+        DpadX = Input.GetAxis("Dpad X");
+        DpadY = Input.GetAxis("Dpad Y");
+
         if (WarpManager.IsWarperActive())
         {
             //return = exiting out of this function
@@ -320,17 +326,17 @@ public class PlayerController : MonoBehaviour
             NewWeaponState = WeaponState.Default;
             source.PlayOneShot(maskSwitchClip);
         }
-        if (Input.GetKeyDown("2"))
+        if (Input.GetAxis("Dpad X") == -1)
         {
             NewWeaponState = WeaponState.Vista;
             source.PlayOneShot(maskSwitchClip);
         }
-        if (Input.GetKeyDown("3"))
+        if (Input.GetAxis("Dpad Y") == 1)
         {
             NewWeaponState = WeaponState.Anchor;
             source.PlayOneShot(maskSwitchClip);
         }
-        if (Input.GetKeyDown("4"))
+        if (Input.GetAxis("Dpad Y") == -1)
         {
             NewWeaponState = WeaponState.Shield;
             source.PlayOneShot(maskSwitchClip);
