65baf70db0b7aecec26b1b84fd46c9e7cf551ac1
Willie Macfarlane <wcmacfarlane@gmail.com>
2020-02-13 03:53:17 -0500

    Crosshair PC Functionality
    hold rmb

---
+++ TechnicRangerVS/Assets/Scripts/ReticleToggle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReticleToggle : MonoBehaviour
+{
+
+    public Image img;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        img.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            img.enabled = true;
+        }
+        else
+        {
+            img.enabled = false;
+        }
+    }
+}
