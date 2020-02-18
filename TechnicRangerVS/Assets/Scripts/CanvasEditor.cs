59b3c28f6a6a5bb6f9a4711b3ea3889cf31c6b44
MrJuniorice <43075764+MrJuniorice@users.noreply.github.com>
2020-02-18 17:13:05 -0500

    Menu Edits

---
+++ TechnicRangerVS/Assets/Scripts/CanvasEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasEditor : MonoBehaviour
+{
+    public GameObject FirstCanvas;
+    public GameObject SecondCanvas;
+    public GameObject ThirdCanvas;
+
+    public void Editor()
+
+    {
+        if (FirstCanvas.gameObject.activeInHierarchy == false)
+        {
+            FirstCanvas.gameObject.SetActive(true);
+            SecondCanvas.gameObject.SetActive(false);
+            ThirdCanvas.gameObject.SetActive(false);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            FirstCanvas.gameObject.SetActive(false);
+            SecondCanvas.gameObject.SetActive(true);
+            ThirdCanvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+
+        else
+            {
+                FirstCanvas.gameObject.SetActive(false);
+                SecondCanvas.gameObject.SetActive(false);
+                ThirdCanvas.gameObject.SetActive(true);
+                Time.timeScale = 1;
+            }
+       
+        }
+}
+}
