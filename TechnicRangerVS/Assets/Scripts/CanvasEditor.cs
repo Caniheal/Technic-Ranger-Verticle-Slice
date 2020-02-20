b273c59eefbdfe6e16bbf144bcf112b4fc5ce1d3
MrJuniorice <43075764+MrJuniorice@users.noreply.github.com>
2020-02-19 19:48:08 -0500

    Setting Menu Added to pause menu UI in each scene

--- TechnicRangerVS/Assets/Scripts/CanvasEditor.cs
+++ TechnicRangerVS/Assets/Scripts/CanvasEditor.cs
@@ -6,34 +6,23 @@ public class CanvasEditor : MonoBehaviour
 {
     public GameObject FirstCanvas;
     public GameObject SecondCanvas;
-    public GameObject ThirdCanvas;
 
-    public void Editor()
-
+    public void Pause()
     {
-        if (FirstCanvas.gameObject.activeInHierarchy == false)
         {
-            FirstCanvas.gameObject.SetActive(true);
-            SecondCanvas.gameObject.SetActive(false);
-            ThirdCanvas.gameObject.SetActive(false);
-            Time.timeScale = 0;
-        }
-        else
-        {
-            FirstCanvas.gameObject.SetActive(false);
-            SecondCanvas.gameObject.SetActive(true);
-            ThirdCanvas.gameObject.SetActive(false);
-            Time.timeScale = 1;
-        }
-
-        else
+            if (FirstCanvas.gameObject.activeInHierarchy == false)
+            {
+                FirstCanvas.gameObject.SetActive(true);
+                SecondCanvas.gameObject.SetActive(false);
+                Time.timeScale = 0;
+            }
+            else
             {
                 FirstCanvas.gameObject.SetActive(false);
-                SecondCanvas.gameObject.SetActive(false);
-                ThirdCanvas.gameObject.SetActive(true);
+                SecondCanvas.gameObject.SetActive(true);
                 Time.timeScale = 1;
             }
-       
         }
-}
+   
+    }
 }
