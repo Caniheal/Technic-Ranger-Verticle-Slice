54f5d2eb86ec23c90b88212809ccc501fc98fc7d
MrJuniorice <43075764+MrJuniorice@users.noreply.github.com>
2020-02-20 16:14:01 -0500

    Added Gamma Script
    Not fully functional

---
+++ TechnicRangerVS/Assets/Scripts/Gamma.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamma : MonoBehaviour
+{
+    public float rgbValue = .5f;
+
+    // Update is called once per frame
+    public void ChangeLight(float rgbGamma)
+    {
+        rgbValue = rgbGamma;
+        RenderSettings.ambientLight = new Color(rgbValue, rgbValue, rgbValue, 1);
+    }
+}
