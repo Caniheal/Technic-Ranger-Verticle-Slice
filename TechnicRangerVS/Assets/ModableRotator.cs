df8399d3ebe36b5cc6ff3312ae073326fb96db14
Willie Macfarlane <wcmacfarlane@gmail.com>
2020-02-22 16:35:17 -0500

    Axis Indicator and Menu jazz

---
+++ TechnicRangerVS/Assets/ModableRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModableRotator : MonoBehaviour
+{
+    public int xAngle, yAngle, zAngle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(xAngle, yAngle, zAngle) * Time.deltaTime);
+    }
+}
