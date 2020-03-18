5839cb96ace9a26bb8858d560c7d4017540fa1bc
Nora Barnes <cauctrott@gmail.com>
2020-03-18 18:41:07 -0400

    walking sounds

---
+++ TechnicRangerVS/Assets/Scripts/AnimationEventsStep.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventsStep : MonoBehaviour
+{
+    [SerializeField]
+    public AudioClip[] steps;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void Step ()
+    {
+        AudioClip step = GetRandomClip();
+        audioSource.PlayOneShot(step);
+    }
+    private AudioClip GetRandomClip()
+    {
+        return steps[UnityEngine.Random.Range(0, steps.Length)];
+    }
+}
