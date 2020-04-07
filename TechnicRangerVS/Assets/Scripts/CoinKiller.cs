0b0455b269ecdb8996bd7a061f9a9f2e3aaa2e50
MrJuniorice <43075764+MrJuniorice@users.noreply.github.com>
2020-04-07 17:20:17 -0400

    Added UI for Coin pickup

--- TechnicRangerVS/Assets/Scripts/CoinKiller.cs
+++ TechnicRangerVS/Assets/Scripts/CoinKiller.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinKiller : MonoBehaviour
 {
     public AudioSource sound;
     public Collider coinBox;
     public GameObject diamond, ring;
-    
+    public Text countText;
+    private int count;
+
 
     private void Start()
     {
         sound = GetComponent<AudioSource>();
         coinBox.enabled = true;
+        count = 0;
+        SetCountText();
     }
 
     
@@ -31,14 +36,23 @@ public class CoinKiller : MonoBehaviour
         coinBox.enabled = false;
             sound.Play();
 
+        count = count + 1;
+        SetCountText();
 
-        
 
         Object.Destroy(gameObject, .15f);
 
      
             
         }
-   
+    void SetCountText()
+    {
+        countText.text = "Coins Collected: " + count.ToString();
+        if (count >= 12)
+        {
+            
+        }
+    }
+
 }
 
